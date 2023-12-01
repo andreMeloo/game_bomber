9e8b0c0bfe311c28e07fbffb8d149cd376e9426c
Andre Melo <programador.sly@gmail.com>
2023-12-01 20:24:37 -0300

    fix: Package Modification

--- desktop/src/com/game/DesktopLauncher.java
+++ desktop/src/com/game/DesktopLauncher.java
@@ -1,8 +1,8 @@
-package com.bomber.game;
+package com.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.bomber.game.bomber;
+import com.game.controller.GameControl;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Bomber");
-		new Lwjgl3Application(new bomber(), config);
+		new Lwjgl3Application(new GameControl(), config);
 	}
 }
